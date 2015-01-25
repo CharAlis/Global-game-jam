cf41e31c8e8af6119c16113a742465dafe127552
AntonDan <anton.dan@hotmail.com>
2015-01-25 07:07:30 +0200

    Squasher Added

---
+++ Now_What 2/Assets/Scripts/Squasher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Squasher : MonoBehaviour {
+
+	public float fallSpeed = 30f;
+	public float riseSpeed = 10f;
+	public float waitTime = 2f;
+
+
+	IEnumerator Cycle() {
+		while ( ) {
+
+		}
+		yield return null;
+	}
+}
